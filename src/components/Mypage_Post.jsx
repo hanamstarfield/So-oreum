021f53e9ea1df36eaa670e88f089e3df5e1b2040
za0012 <zinzeek@gmail.com>
2024-09-13 20:45:58 +0900

    마이페이지 틀

---
+++ src/components/Mypage_Post.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Mypage_Post = () => {
+    const PostData = async () => {
+        
+    }
+  return (
+    <div>Mypage_Post</div>
+  )
+}
+
+export default Mypage_Post
--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,5 +1,25 @@
+import { useEffect } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import useUserStore from "../zustand/useUserStore";
+import '../css/Mypage.css'
+import cow1 from "../img/myPage1.png";
+import cow2 from "../img/myPage2.png";
+
 const Mypage = () => {
-    return <div>Mypage</div>;
+    const { user, logOutUser } = useUserStore((state) => state);
+    console.log(user)
+
+    return (
+        <>
+            <div className="main_Container">
+                <div className="Profile">
+                    <img src={cow1} alt="소오름 프로필" className="cowProfile"/>
+                    <div>{user.nickname}</div>
+                </div>
+                <div>게시글</div>
+            </div>
+        </>
+    );
 };
 
 export default Mypage;
