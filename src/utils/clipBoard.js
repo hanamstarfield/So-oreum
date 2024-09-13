3c23d1c1dc0d880aee1224d737ef946b7d486c07
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-13 16:28:08 +0900

    refactor: 빨간줄 삭제 (안쓰는 import 등)

--- src/utils/clipBoard.js
+++ src/utils/clipBoard.js
@@ -3,6 +3,7 @@ const handleCopyClipBoard = async (text) => {
     await navigator.clipboard.writeText(text);
     alert('복사 성공! ' + text);
   } catch (error) {
+    console.error(error);
     alert('복사 실패!');
   }
 };
