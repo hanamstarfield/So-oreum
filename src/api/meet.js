7529a0df9c5a0fb19667f0da372619321d735155
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-13 20:44:49 +0900

    feat: 벙개 작성 페이지 진행중 (merge전 커밋)

--- src/api/meet.js
+++ src/api/meet.js
@@ -8,10 +8,15 @@ const getSpeedMeetById = async (id) => {
   return (await api.get(`?id=${id}`)).data[0];
 }
 
+const getDummyMountain = async () => {
+  const { data } = await axios.get(`http://localhost:4000/dummyMnt`);
+  return data;
+}
 
 const meetApi = {
   getSpeedMeets,
-  getSpeedMeetById
+  getSpeedMeetById,
+  getDummyMountain
 }
 
 const api = axios.create({
