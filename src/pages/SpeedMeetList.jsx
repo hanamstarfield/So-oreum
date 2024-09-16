c2596bdf847a51d501197be6783e15feae5c6fa8
LeeJY97 <ijoonyeol97@gmail.com>
2024-09-16 14:13:20 +0900

    feat: 페이지 처리 (버튼처리 미완)

--- src/pages/SpeedMeetList.jsx
+++ src/pages/SpeedMeetList.jsx
@@ -3,21 +3,26 @@ import { Link, useParams } from "react-router-dom";
 
 const SpeedMeetList = () => {
     const { page } = useParams();
-    const { data, isPending, fetchNextPage, hasNextPage, isFetchingNextPage } = useGetSpeedMeetsQuery(page);
+    const { data, isPending } = useGetSpeedMeetsQuery(page);
 
     if (isPending) {
         return <>... 로딩</>;
     }
 
+    console.log("data", data);
+
     // console.log("data", data);
     // console.log("fetchNextPage", fetchNextPage);
     // console.log("hasNextPage", hasNextPage);
     // console.log("isFetchingNextPage", isFetchingNextPage);
-    const speedMeetList = data.pages[0].data;
+    const speedMeetList = data.data;
+    // const last = data.pages[0].last;
     // console.log("speedMeetList", speedMeetList);
 
+    console.log("speedMeetList", speedMeetList);
+
     return (
-        <div className="h-lvh bg-[#214A00] flex justify-center items-center">
+        <div className="h-lvh bg-[#214A00] flex justify-center items-center relative">
             <Link to="/speed-meet-write">
                 <button className="fixed top-48 right-10 bg-lime-300">작성</button>
             </Link>
@@ -32,6 +37,23 @@ const SpeedMeetList = () => {
                     );
                 })}
             </ul>
+            <div className="absolute w-[60%] h-[30px] bottom-20 bg-slate-600 flex justify-center gap-4">
+                <Link to="/speed-meet/1">
+                    <span>1</span>
+                </Link>
+                <Link to="/speed-meet/2">
+                    <span>2</span>
+                </Link>
+                <Link to="/speed-meet/3">
+                    <span>3</span>
+                </Link>
+                <Link to="/speed-meet/4">
+                    <span>4</span>
+                </Link>
+                <Link to="/speed-meet/5">
+                    <span>5</span>
+                </Link>
+            </div>
         </div>
     );
 };
--- src/queries/useGetSpeedMeetsQuery.js
+++ src/queries/useGetSpeedMeetsQuery.js
@@ -2,21 +2,16 @@ import meetApi from '@/api/meet';
 import queryKey from './queryKeys'
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
+
 const useGetSpeedMeetsQuery = (pageParam) => {
-  console.log('pageParam', pageParam);
-  return useInfiniteQuery({
-    queryKey: [queryKey.default.speedMeets],
-    queryFn: () => meetApi.getSpeedMeets({ pageParam }),
-    // pages : 누적 (무한 스크롤경우)
-    // lastPAge: 현재
-    getNextPageParam: (lastPage, pages) => {
-      console.log('lastPage', lastPage);
-      console.log('pages', pages);
-      return lastPage.nextPage ?? false;
-    },
+  console.log('useQuery  pageParam => ', pageParam);
+  return useQuery({
+    queryKey: [queryKey.default.speedMeets(pageParam)],
+    queryFn: () => meetApi.getSpeedMeets(pageParam),
     onError: (error) => {
       alert(error);
-    }
+    },
+    staleTime: 0,
   });
 }
 
