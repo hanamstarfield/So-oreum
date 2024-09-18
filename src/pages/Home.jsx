eda987a88891fb5ced2272c3ce80da4d39ed7371
za0012 <zinzeek@gmail.com>
2024-09-18 19:11:30 +0900

    임시저장

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,4 @@
 import KakaoMap from "@/components/KakaoMap";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-
-const Home = () => {
-    return (
-        <div><div>Home</div>
-            <KakaoMap /><Link to="/speed-meet">
-                <span>speed-meet 바로가기 (임시)</span>
-            </Link>
-        </div>
-    )
-}
-=======
 
 const Home = () => {
     return (
@@ -22,5 +9,4 @@ const Home = () => {
     );
 };
 
->>>>>>> 67ec4577f1371d596fc691b2bf8cc74792f03e12
 export default Home;
