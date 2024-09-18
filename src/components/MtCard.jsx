678e676ae608c1d50c888c623489d36f318b8223
hanamstarfield <qnfdmlto@naver.com>
2024-09-18 12:19:48 +0900

    feat: Mt 리스트 기본 구성

---
+++ src/components/MtCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MtCard = () => {
+    return <div>MtCard</div>;
+};
+
+export default MtCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import KakaoMap from "@/components/KakaoMap";
+import MtCard from "@/components/MtCard";
+import axios from "axios";
 
 const Home = () => {
+    const fetchMount = async () => {
+        const response = await axios.get("http://localhost:4000/items");
+        return response.data;
+    };
+
     return (
         <div>
-            <KakaoMap />
+            <MtCard fetchMount={fetchMount} />
+            <KakaoMap fetchMount={fetchMount} />
             <span>speed-meet 바로가기 (임시)</span>
         </div>
     );
