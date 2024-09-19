10c5b78196538b699647b51f0bc7d1e71a55a223
Subin, Han <ella0411@naver.com>
2024-09-19 21:00:13 +0900

    feat: 해당 산 벙개 목록 렌더링

--- src/components/MtCard.jsx
+++ src/components/MtCard.jsx
@@ -1,7 +1,22 @@
 import { useNavigate } from "react-router-dom";
-import MtCardDefaultImg from "@/assets/MtCardDefault.png";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import MtCardDefaultImg from "@/assets/MtCardDefault.jpg";
+import LightningImg from "@/assets/Lightning.png";
 
 const MtCard = ({ mount, latlng }) => {
+    const API_URL = "http://localhost:4000/speedMeets";
+
+    const fetchData = async () => {
+        const response = await axios.get(API_URL);
+        return response.data;
+    };
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ["speedMeets"],
+        queryFn: fetchData
+    });
+
     const navigate = useNavigate();
     const { swLatlng, nwLatlng } = latlng;
 
@@ -13,6 +28,15 @@ const MtCard = ({ mount, latlng }) => {
             nwLatlng.Ma >= item.latitude
     );
 
+    // const filteredSPList = data.filter((sp) => item.mntnid === sp.mntnid);
+
+    if (isLoading) {
+        return <div>로딩 중입니다...</div>;
+    }
+
+    if (isError) {
+        return <div>데이터 조회 중 오류가 발생했습니다...</div>;
+    }
     return (
         <div className="flex flex-wrap w-[800px] h-[800px] leading-loose overflow-y-scroll cursor-pointer">
             {filteredMntns.length > 0 ? (
@@ -22,6 +46,10 @@ const MtCard = ({ mount, latlng }) => {
                             className="w-[250px]  bg-white rounded-[15px] p-[15px] my-[10px] mx-[5px] box-contents brightness-100  hover:brightness-90 duration-100"
                             onClick={() => navigate(`/mntn-detail?mntnId=${item.mntnid}`)}
                         >
+                            <div className="flex justify-end items-center px-[10px] text-[13px]">
+                                <img src={LightningImg} className="w-[20px] h-[20px] object-cover" />
+                                <span> {data.filter((sp) => item.mntnid === sp.mntnid).length}</span>
+                            </div>
                             <img
                                 className="w-[220px] h-[150px] object-cover rounded-[15px]"
                                 src={item.mntnattchimageseq}
--- src/pages/MtDetail.jsx
+++ src/pages/MtDetail.jsx
@@ -1,34 +1,56 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import LightningImg from "@/assets/Lightning.png";
+import SpCardDefaultImg from "@/assets/SpCardDefault.jpg";
 
 const MtDetail = () => {
+    const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
     const mntnId = searchParams.get("mntnId");
 
-    const API_URL = "http://localhost:4000/items";
+    const MT_API_URL = "http://localhost:4000/items";
 
-    const fetchData = async () => {
-        const response = await axios.get(API_URL);
+    const mtFetchData = async () => {
+        const response = await axios.get(MT_API_URL);
         return response.data;
     };
 
-    const { data, isLoading, isError } = useQuery({
+    const List_API_URL = "http://localhost:4000/speedMeets";
+
+    const listFetchData = async () => {
+        const response = await axios.get(List_API_URL);
+        return response.data;
+    };
+
+    const {
+        data: list,
+        isLoading: listIsLoading,
+        isError: listIsError
+    } = useQuery({
+        queryKey: ["speedMeets"],
+        queryFn: listFetchData
+    });
+    const {
+        data: mntn,
+        isLoading: mntnIsLoading,
+        isError: mntnIsError
+    } = useQuery({
         queryKey: ["items"],
-        queryFn: fetchData
+        queryFn: mtFetchData
     });
 
-    if (isLoading) {
+    if (mntnIsLoading || listIsLoading) {
         return <div>로딩 중입니다...</div>;
     }
 
-    if (isError) {
+    if (mntnIsError || listIsError) {
         return <div>데이터 조회 중 오류가 발생했습니다...</div>;
     }
 
     return (
         <div>
-            {data.map((item) => {
+            {mntn.map((item) => {
                 if (item.mntnid === Number(mntnId)) {
                     return (
                         <div classNamekey={item.mntnid}>
@@ -57,7 +79,7 @@ const MtDetail = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex flex-col justify-center items-start m-[auto] w-[900px] text-justify mt-[50px] gap-[50px] leading-loose">
+                            <div className="flex flex-col justify-center items-start m-[auto] w-[900px] text-justify my-[50px] gap-[50px] leading-loose">
                                 <p>
                                     <strong>개관</strong>
                                     <br />
@@ -68,6 +90,47 @@ const MtDetail = () => {
                                     <br />
                                     {item.mntninfodtlinfocont}
                                 </p>
+                                <div className="mt-[20px]">
+                                    <div className="flex items-center mb-[20px]">
+                                        <img src={LightningImg} className="w-[30px] h-[30px] object-cover" />
+
+                                        <h3>
+                                            <strong>벙개 목록</strong>
+                                        </h3>
+                                    </div>
+
+                                    <div>
+                                        {list
+                                            .filter((sp) => item.mntnid === sp.mntnid)
+                                            .map((sp) => {
+                                                return (
+                                                    <div
+                                                        className="flex flex-col justify-between w-[300px] h-[200px] border-[3px] rounded-[30px] p-[10px] box-contents bg-white brightness-100  hover:brightness-90 duration-100 cursor-pointer"
+                                                        key={sp.id}
+                                                        onClick={() => navigate(`/speed-meet-detail/${sp.id}`)}
+                                                    >
+                                                        <div className="flex justify-between px-[10px]">
+                                                            {/* <p className="text-[12px] text-start w-[100%]">
+                                                            방장: {sp.userId}
+                                                        </p> */}
+                                                            <p className="text-[12px] ">{sp.title}</p>
+                                                            <p className="text-[12px]">
+                                                                {sp.attendance}/{sp.capacity}
+                                                            </p>
+                                                        </div>
+
+                                                        <img
+                                                            src={SpCardDefaultImg}
+                                                            className="flex m-[auto] w-[100%] h-[130px] object-cover rounded-[30px]"
+                                                        />
+                                                        <div className="flex justify-end px-[10px]">
+                                                            <p className="text-[12px]">{sp.date}</p>
+                                                        </div>
+                                                    </div>
+                                                );
+                                            })}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     );
